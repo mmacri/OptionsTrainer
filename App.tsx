5690d39832a79565448b8f80555dd4bf9b08623a
mmacri <mikemacri@gmail.com>
2025-08-29 16:19:06 -0700

    Add build pipeline and static site

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { InteractiveOptionsChart } from './components/InteractiveOptionsChart';
-import './styles/globals.css';
 
 export default function App() {
   return <InteractiveOptionsChart />;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './index.html',
+    './App.tsx',
+    './components/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
