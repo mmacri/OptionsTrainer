37b2cea27fb5dda4830e79deed1f4e350d950adf
mmacri <mikemacri@gmail.com>
2025-08-30 09:45:04 -0700

    refactor: remove redundant root rendering

--- main.tsx
+++ main.tsx
@@ -1,21 +1,8 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const container = document.getElementById('root');
-if (container) {
-  const root = createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
-
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
